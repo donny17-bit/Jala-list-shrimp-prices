1a8c80d126b1521ad194f21a2ff6cc8e0757a7db
Donny <doniwahyu14@gmail.com>
2023-03-30 17:14:28 +0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import Filter from './components/Filter.vue';
+</script>
+
+<template>
+  <Filter />
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/Filter.vue
@@ -0,0 +1,7 @@
+<script setup>
+
+</script>
+
+<template>
+    <p class="text-gray-600 font-semibold">Filter</p>
+</template>
